eb2cd47f28ae1ae8e58a23ff94d9db6ecf7983d9
Sai To & Evan <saito+evan@appacademy.io>
2014-07-30 11:25:06 -0400

    Create stuff

---
+++ app/models/cat.rb
@@ -0,0 +1,7 @@
+class Cat < ActiveRecord::Base
+  validates :age, numericality: true
+  validates :color, inclusion: %w(brown black white blue red pink)
+  validates :sex, inclusion: %w(M F)
+  validates :age, :birth_date, :color, :name, :sex, presence: true
+
+end
